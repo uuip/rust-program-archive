5205f758d405657d3a0b0cf6cb7c383b886e1e68
uuip <uuip@users.noreply.github.com>
2025-01-29 15:52:04 +0800

    update

--- common/src/setting.rs
+++ common/src/setting.rs
@@ -1,4 +1,7 @@
-#[derive(Debug)]
+use dotenvy::dotenv;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
 pub struct Setting {
     pub pulsar_addr: String,
     pub topic: String,
@@ -7,26 +10,9 @@ pub struct Setting {
     pub rpc_list: Vec<String>,
 }
 
-pub fn get_env(key: &str) -> String {
-    dotenvy::var(key).unwrap_or_else(|_| panic!("lost {key}"))
-}
-
 impl Setting {
     pub fn init() -> Self {
-        let pulsar_addr = get_env("PULSAR_URL");
-        let topic = get_env("PULSAR_TOPIC");
-        let explorer_db = get_env("EXPLORER_DB");
-        let db = get_env("DB_URL");
-        let rpc_list = get_env("rpc_list")
-            .split(',')
-            .map(str::to_string)
-            .collect::<Vec<String>>();
-        Self {
-            pulsar_addr,
-            topic,
-            explorer_db,
-            db,
-            rpc_list,
-        }
+        dotenv().ok();
+        envy::from_env::<Setting>().unwrap()
     }
 }
